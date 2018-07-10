7878f0152ea1b67f436c833571c696db5f43810e
tatsu <tatuo0228@icloud.com>
2018-07-10 04:08:58 +0000

    bootstrap design

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
   
   def index
-    @messages = Message.all
+    @messages = Message.all.page(params[:page])
   end
 
   def show
@@ -28,7 +28,6 @@ class MessagesController < ApplicationController
   end
 
   def update
-
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
